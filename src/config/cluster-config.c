dd6dda7b1d1a07b31188c864c209929a76b3400a
Nico Stefani <nicolas.stefi@wazuh.com>
2024-03-18 16:24:48 -0300

    Add validations for HAProxy helper parameters

--- src/config/cluster-config.c
+++ src/config/cluster-config.c
@@ -30,6 +30,20 @@ int Read_Cluster(XML_NODE node, void *d1, __attribute__((unused)) void *d2) {
     static const char *bind_addr = "bind_addr";
     static const char *C_VALID = "!\"#$%&'-.0123456789:<=>?ABCDEFGHIJKLMNOPQRESTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~";
 
+    xml_node **children = NULL;
+    static const char *haproxy_helper = "haproxy_helper";
+    static const char *haproxy_address = "haproxy_address";
+    static const char *haproxy_port = "haproxy_port";
+    static const char *haproxy_user = "haproxy_user";
+    static const char *haproxy_password = "haproxy_password";
+    static const char *excluded_nodes = "excluded_nodes";
+    static const char *frequency = "frequency";
+    static const char *agent_chunk_size = "agent_chunk_size";
+    static const char *agent_reconnection_time = "agent_reconnection_time";
+    static const char *agent_reconnection_stability_time = "agent_reconnection_time";
+    static const char *imbalance_tolerance = "imbalance_tolerance";
+    static const char *remove_disconnected_node_after = "remove_disconnected_node_after";
+
     _Config *Config;
     Config = (_Config *)d1;
     int i;
@@ -96,6 +110,46 @@ int Read_Cluster(XML_NODE node, void *d1, __attribute__((unused)) void *d2) {
         } else if (!strcmp(node[i]->element, nodes)) {
         } else if (!strcmp(node[i]->element, port)) {
         } else if (!strcmp(node[i]->element, bind_addr)) {
+        } else if (!strcmp(node[i]->element, haproxy_helper)) {
+            children = OS_GetElementsbyNode(xml, nodes[i])
+
+            if (!children) {
+                continue;
+            }
+
+            for (j = 0; children[j]; j++) {
+                if (!strcmp(children[j]->child, disabled)) {
+                        if (strcmp(children[j]->child, "yes") && strcmp(children[j]->child, "no")) {
+                        merror("Detected a not allowed value for disabled tag '%s'. Valid values are 'yes' and 'no'.", children[j]->child);
+                        return OS_INVALID;
+                    }
+                } else if (!strcmp(children[j]->child, frequency) {
+
+                } else if (!strcmp(children[j]->child, haproxy_address) {
+
+                } else if (!strcmp(children[j]->child, haproxy_port) {
+
+                } else if (!strcmp(children[j]->child, haproxy_user) {
+
+                } else if (!strcmp(children[j]->child, haproxy_password) {
+
+                } else if (!strcmp(children[j]->child, excluded_nodes) {
+
+                } else if (!strcmp(children[j]->child, agent_chunk_size) {
+
+                } else if (!strcmp(children[j]->child, agent_reconnection_time) {
+
+                } else if (!strcmp(children[j]->child, agent_reconnection_stability_time) {
+
+                } else if (!strcmp(children[j]->child, imbalance_tolerance) {
+
+                } else if (!strcmp(children[j]->child, remove_disconnected_node_after) {
+
+                } else {
+                    merror(XML_INVELEM, children[i]->child);
+                    return OS_INVALID;
+                }
+
         } else {
             merror(XML_INVELEM, node[i]->element);
             return OS_INVALID;
@@ -107,3 +161,21 @@ int Read_Cluster(XML_NODE node, void *d1, __attribute__((unused)) void *d2) {
 
     return 0;
  }
+
+// <cluster>
+//     <haproxy_helper>
+//       <disabled>yes</disabled>
+//       <frequency>60</frequency>
+//       <haproxy_address></haproxy_address>
+//       <haproxy_port></haproxy_port>
+//       <haproxy_user></haproxy_user>
+//       <haproxy_password></haproxy_password>
+//       <excluded_nodes>
+//         <node>master</node>
+//       </excluded_nodes>
+//       <agent_chunk_size>100</agent_chunk_size>
+//       <agent_reconnection_time>5</agent_reconnection_time>
+//       <imbalance_tolerance>10</imbalance_tolerance>
+//       <remove_disconnected_node_after>60</remove_disconnected_node_after>
+//     </haproxy_helper>
+// </cluster>
